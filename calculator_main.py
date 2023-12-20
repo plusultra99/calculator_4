57f82cba8567f73ce3b8ef330aab0c0fcedb7780
plusultra99 <149741645+plusultra99@users.noreply.github.com>
2023-12-20 18:36:21 +0900

    GUI 수정 및 개선 #4
    숫자 입력 / 표시 부분 통합 #1

--- calculator_main.py
+++ calculator_main.py
@@ -16,14 +16,11 @@ class Main(QDialog):
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Solution: ")
+        label_equation = QLabel("")
         self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
         layout_equation_solution.addRow(label_equation, self.equation)
-        layout_equation_solution.addRow(label_solution, self.solution)
 
         ### 사칙연상 버튼 생성
         button_plus = QPushButton("+")
@@ -104,12 +101,12 @@ class Main(QDialog):
 
     def button_equal_clicked(self):
         equation = self.equation.text()
-        solution = eval(equation)
-        self.solution.setText(str(solution))
+        equation = eval(equation)
+        self.equation.setText(str(equation))
 
     def button_clear_clicked(self):
         self.equation.setText("")
-        self.solution.setText("")
+        
 
     def button_backspace_clicked(self):
         equation = self.equation.text()
